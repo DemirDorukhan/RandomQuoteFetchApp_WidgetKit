d0f73f2c50a199d9b4a5030a19318eaf27fba0ef
Dorukhan Demir <cryptodorko@gmail.com>
2023-03-30 17:13:50 +0300

    Initial Commit

---
+++ D'quoteNew1/D_quoteNew1App.swift
@@ -0,0 +1,17 @@
+//
+//  D_quoteNew1App.swift
+//  D'quoteNew1
+//
+//  Created by Dorukhan Demir on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct D_quoteNew1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
